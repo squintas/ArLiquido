087ca203f27e2aec1c21287d22d8cb4611c18a1a
Ricardo Quintas <rdquintas@yahoo.com>
2021-04-12 19:59:52 +0100

    Animation all cards

--- v04_proposta/js/main.js
+++ v04_proposta/js/main.js
@@ -1,150 +1,45 @@
-// // Add scrollmagic controller
-// let controller = new ScrollMagic.Controller();
+var _aCards;
+var _oLogo;
+var _speedMarker1;
+var _speedMarker2;
 
-
-
-// //------------------
-// //TIMELINE 2
-// //------------------
-
-// // Add timeline
-// let tl2 = anime.timeline({ autoplay: false });
-
-// // Add animations
-// let s2a1 = {
-//     targets: '#al-animate-this',
-//     // opacity: [0.3, 1],
-//     scale: [1, 4],
-//     duration: 1000,
-//     delay: 0,
-//     easing: 'easeInOutSine'
-// };
-
-// let s2a2 = {
-//     targets: '#al-animate-this',
-//     scale: 1,
-//     duration: 1000,
-// };
-
-// // Add children
-// tl2.add(s2a1)
-// // .add(s2a2);
-
-// // Get section height
-// let twoHeight = document.getElementById("al-animate-this").clientHeight;
-// console.log('twoHeight: ' + twoHeight);
-
-// //------------------
-// //SCENE 2
-// //------------------
-
-// //Add second scrollmagic scene
-// let scene2 = new ScrollMagic.Scene({
-//     triggerElement: "#al-animate-this",
-//     duration: 4500,
-//     triggerHook: 0,
-// })
-
-//     // Add debug indicators
-//     .addIndicators({
-//         colorTrigger: "black",
-//         colorStart: "blue",
-//         colorEnd: "red",
-//         indent: 10
-//     })
-
-//     // Trigger animation timeline
-//     //Use scroll position to play animation
-//     .on("progress", function (event) {
-//         console.log("tl2.duration: " + tl2.duration);
-//         console.log("event.progress: " + event.progress);
-//         tl2.seek(tl2.duration * event.progress);
-//     })
-
-//     .setPin('#al-animate-this')
-//     .addTo(controller);
-
-
-// document.onreadystatechange = () => {
-// if (document.readyState === 'complete') {
-//     const elements = document.querySelectorAll('*[data-parallax]');
-//     Array.from(elements).forEach((el) => {
-//         Parallax.init(el);
-//     });
-// }
-// };
-
-
-
-function getScrollPercent() {
-    var h = document.documentElement,
-        b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-}
-
-const oElement = document.querySelector("#al-animate-this");
-const oLogo = document.querySelector(".al-logo");
-
-const tl = anime.timeline({ autoplay: false });
-
-
-
-tl.add({
-    targets: oElement,
-    // translateX: anime.random(-500, 500) + '%',
-    translateX: 50,
-    // scale: anime.random(0.3, 1.7),
-    // rotate: anime.random(-365, 365) + 'deg',
-    duration: 500,
-    // duration: 3500, //anime.random(500, 5000),
-    // direction: 'alternate',
-    easing: "easeInOutSine"
-}).add({
-    targets: oElement,
-    // translateX: anime.random(-500, 500) + '%',
-    translateX: 0,
-    // scale: anime.random(0.3, 1.7),
-    // rotate: anime.random(-365, 365) + 'deg',
-    duration: 500,
-    // duration: 3500, //anime.random(500, 5000),
-    // direction: 'alternate',
-    easing: "easeInOutSine"
-});
-// }
-
-// _.map(els, el => {
-//     anime.set(el, {
-//         top: anime.random(0, 150) + 'vh',
-//         left: anime.random(0, 100) + 'vw'
-//     });
-
-//     tl.add({
-//         targets: el,
-//         translateX: anime.random(-500, 500) + '%',
-//         translateY: anime.random(-500, 500) + '%',
-//         scale: anime.random(0.3, 1.7),
-//         rotate: anime.random(-365, 365) + 'deg',
-//         duration: anime.random(500, 5000), easing: 'easeInOutCirc'
-//     },
-//         0);
+// const oElement = $("#al-animate-this")[0];
+// const tl = anime.timeline({ autoplay: false });
+// tl.add({
+//     targets: oElement,
+//     // translateX: anime.random(-500, 500) + '%',
+//     translateX: 50,
+//     // scale: anime.random(0.3, 1.7),
+//     // rotate: anime.random(-365, 365) + 'deg',
+//     duration: 500,
+//     // duration: 3500, //anime.random(500, 5000),
+//     // direction: 'alternate',
+//     easing: "easeInOutSine"
+// }).add({
+//     targets: oElement,
+//     // translateX: anime.random(-500, 500) + '%',
+//     translateX: 0,
+//     // scale: anime.random(0.3, 1.7),
+//     // rotate: anime.random(-365, 365) + 'deg',
+//     duration: 500,
+//     // duration: 3500, //anime.random(500, 5000),
+//     // direction: 'alternate',
+//     easing: "easeInOutSine"
 // });
+// // }
 
-
-function collision(oElement, oLogo) {
+function getColisionPercentage(oElement) {
     oElement = $(oElement);
-    oLogo = $(oLogo);
     var x1 = oElement.offset().left;
     var y1 = oElement.offset().top;
     var h1 = oElement.outerHeight(false);
     var w1 = oElement.outerWidth(true);
     var b1 = y1 + h1;
     var r1 = x1 + w1;
-    var x2 = oLogo.offset().left;
-    var y2 = oLogo.offset().top;
-    var h2 = oLogo.outerHeight(false);
-    var w2 = oLogo.outerWidth(true);
+    var x2 = _oLogo.offset().left;
+    var y2 = _oLogo.offset().top;
+    var h2 = _oLogo.outerHeight(false);
+    var w2 = _oLogo.outerWidth(true);
     var b2 = y2 + h2;
     var r2 = x2 + w2;
 
@@ -157,41 +52,72 @@ function collision(oElement, oLogo) {
     }
 }
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        // var iScrollTop = $(window).scrollTop();
-        // oElement.offsetTop - document.body.scrollTop
-        // var iCalc = Math.abs(iScrollTop - oElement.offsetTop);
-        // console.log(iScrollTop + "-" + oElement.offsetTop + "=" + iCalc);
-        // console.log("oElement.offsetTop: " + oElement.offsetTop);
-        // console.log("iScrollTop: " + iScrollTop);
-        var iPercent;
-        if (iPercent = collision(oElement, oLogo)) {
-            // debugger;
-            tl.seek(iPercent * 10);
+function initializeAnimationForAllCards() {
+    var arr = $(".al-card");
+    _aCards = [];
 
-            // console.log(collision(oElement, oLogo));
-            // console.log("oElement.offsetTop: " + oElement.offsetTop);
-            // console.log("iScrollTop: " + iScrollTop);
+    for (let i = 0; i < arr.length; i++) {
+        var oCard = arr[i];
+        var oTimeLine = anime.timeline({ autoplay: false });
+        oTimeLine.add({
+            targets: oCard,
+            translateX: 50,
+            duration: 500,
+            easing: "easeInOutSine"
+        }).add({
+            targets: oCard,
+            translateX: 0,
+            duration: 500,
+            easing: "easeInOutSine"
+        });
+
+        var obj = {
+            anime: oTimeLine,
+            card: $(oCard)
         }
-        // if (iCalc >= 200 && iCalc <= 260) {
-        // console.log(tl.duration * (iCalc * 0.01));
-        // console.log(iCalc);
-        // }
+
+        _aCards.push(obj);
+    }
+}
+
+function scrollSpeedIsHigh() {
+    _speedMarker1 = _speedMarker2 || 0;
+    _speedMarker2 = $(window).scrollTop();
+    var dif = Math.abs(_speedMarker1 - _speedMarker2);
+    console.log(dif);
+    if (dif > 15) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function animateCard() {
+    var iPercent;
+    for (let i = 0; i < _aCards.length; i++) {
+        var oCard = _aCards[i];
+        if (iPercent = getColisionPercentage(oCard.card)) {
+            oCard.anime.seek(iPercent * 10);
+            break;
+        }
+    }
+}
+
+function allignAllCardsToTheRight() {
+    for (let i = 0; i < _aCards.length; i++) {
+        var oCard = _aCards[i];
+        oCard.card.css("transform", "translateX(0.0px)");
+    }
+}
+
+$(document).ready(function () {
+    _oLogo = $($(".al-logo")[0]);
+    initializeAnimationForAllCards();
+    $(window).scroll(function (oEvent) {
+        if (scrollSpeedIsHigh()) {
+            allignAllCardsToTheRight();
+        } else {
+            animateCard();
+        }
     });
-});
-
-
-window.addEventListener('scroll', () => {
-    // var zrq = document.querySelector("#al-animate-this");
-    // console.log("offsetTop: " + oElement.offsetTop);
-    // console.log("document.body.scrollTop: " + document.body.scrollTop);
-
-    // oElement.offsetTop - document.body.scrollTop;
-
-    // const percentage = getScrollPercent();
-    // console.log("percentage: " + percentage * 100);
-    // console.log("tl.duration: " + tl.duration);
-    // tl.seek(percentage * 100);
-    // tl.seek(tl.duration * (percentage * 0.01));
 });
